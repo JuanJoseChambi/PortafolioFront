29a9ad10045635e7924a0c03e6bdc003f539b73c
Juan Jose <chambijuanjose05@gmail.com>
2023-08-28 20:08:07 -0300

    deploy front

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Portafolio from "./View/Portafolio/Portafolio"
 import UpLoadContent from "./View/UpLoadContent/UpLoadContent"
 import axios from "axios"
-axios.defaults.baseURL="https://portafoliojuanjose.up.railway.app//";
+axios.defaults.baseURL="https://portafoliojuanjose.up.railway.app/";
 
 function App() {
 
